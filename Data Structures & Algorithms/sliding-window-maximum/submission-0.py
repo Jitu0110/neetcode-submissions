6adc3c3121084f2890e1ec190fc9d3a0a9bd207b
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-08-17 00:45:57 -0700

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.py
@@ -0,0 +1,43 @@
+
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        #nums = [1,2,1,0,4,2,6]
+        #q = [1,2,3]
+        #o = [2,]
+
+        #Brute force - O(n * k) solution
+
+        #Monotonically Decreasing Queue
+        output = []
+        queue = deque() #holds indices
+        left = right = 0
+
+        while right < len(nums):
+
+            while queue and nums[queue[-1]] < nums[right]:
+                queue.pop()
+            queue.append(right)
+
+            #remove left val from window
+            if left > queue[0]:
+                queue.popleft()
+
+            if (right + 1) >= k:
+                output.append(nums[queue[0]])
+                left += 1
+            
+            right += 1
+
+        return output
+            
+
+
+
+
+
+        
+
+
+        
