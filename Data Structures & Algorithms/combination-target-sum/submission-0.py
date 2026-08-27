a6f753018f631b41790a0a55a9f2b7e76717f75a
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-08-26 22:25:22 -0700

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.py
@@ -0,0 +1,43 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+        #Time complexity : O(N^(T/M))
+        res = []
+     
+        path = []
+
+        def dfs(i, sum):
+            if sum == target:
+                res.append(path[:])
+                return
+            
+            if sum > target:
+                return
+            
+            for j in range(i, len(nums)):
+                #Take the number in path
+                sum += nums[j]
+                path.append(nums[j])
+
+                dfs(j,sum)
+
+                #Remove the number in path
+                sum -= nums[j]
+                path.pop()
+
+        
+        dfs(0,0)
+
+        return res
+
+
+
+
+
+            
+
+
+
+            
+
+        
