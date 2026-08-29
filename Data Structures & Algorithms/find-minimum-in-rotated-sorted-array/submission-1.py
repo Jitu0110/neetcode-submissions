f16982ebb8ed2525cf328e0cbe182df257e7b88c
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-08-28 22:31:40 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        #time - O(log(n))
+
+        #logic: Using binary search, either l and mid will be in one sorted half OR r and mid will be one sorted half
+        # Questions for if else: Could mid still be the answer? Yes - keep it, else ignore it
+
+
+        #Is mid on the left sorted section or the right sorted section?
+
+
+        left, right = 0, len(nums) - 1
+
+        while(left < right):
+
+            mid = left + (right-left)//2
+
+            if nums[mid] > nums[right]:
+                #solution in right half
+                left = mid + 1
+            else:
+                right = mid
+
+        
+        return nums[left]
+
