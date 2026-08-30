b7311caf03480674717391a02c70dfed53ad64d2
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-08-30 00:08:46 -0700

    Add: partition-equal-subset-sum - submission-4

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        #Time complexity - O(2^n) Brute force
+
+        if sum(nums) % 2 != 0: #Cant partition for odd sum
+            return False
+
+        target = sum(nums)//2
+
+        def dfs_backtrack(i, sum):
+
+            if sum > target:
+                return False
+            
+            if sum == target:
+                return True
+            
+            for j in range(i,len(nums)):
+               if dfs_backtrack(j+1, sum + nums[i]):
+                   return True
+            return False
+
+        return dfs_backtrack(0,0)
+                
+            
+
+
+        
