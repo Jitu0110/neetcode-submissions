6f1a7a9ad8089f3418f81ccf13f5951241d3d2ed
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-08-29 18:40:46 -0700

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,71 @@
+class Solution:
+    # def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+    #     #bottom up
+    #     #Space - O(N) Time - O(N)
+
+    #     dp = [0] * (len(cost) + 1) #[0 0 0 0]
+
+    #     #min cost to reach 0 index - 0
+    #     #min cost to reach 1 index - 0
+
+
+    #     for i in range(2,len(cost) + 1):
+    #         dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]) # 0 0 1 2
+
+
+    #     return dp[-1]
+
+
+    # def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+    #     #Optimized bottom up
+    #     #Space - O(1) Time - O(N)
+
+    #     a = 0
+    #     b = 0 
+    #     c = 0
+
+    #     #min cost to reach 0 index - 0
+    #     #min cost to reach 1 index - 0
+
+
+    #     for i in range(2,len(cost) + 1):
+    #         c = min(b + cost[i-1], a + cost[i-2]) # 0 0 1 2
+    #         a = b
+    #         b = c
+
+
+    #     return c
+
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        #Top down
+        #Space - O(N) Time - O(N)
+        # Space complexity comes from recursion stack as well as cache
+
+
+
+        #min cost to reach 0 index - 0
+        #min cost to reach 1 index - 0
+
+        cache = {}
+
+        def dfs(n):
+            if n == 1 or n == 0:
+                return 0
+            
+            if n in cache:
+                return cache[n]
+            
+            cache[n] = min(dfs(n-1)+cost[n-1], dfs(n-2) + cost[n-2])
+
+            return cache[n]
+        
+        return dfs(len(cost))
+            
+
+
+
+        
