63fce7d481c8224189df420068a40c9a4698122a
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-08-31 23:26:24 -0700

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,86 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution: 
+
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+            #k is number of lists
+            #
+
+        minHeap = []
+
+        dummy = ListNode(0)
+        curr = dummy
+
+        count = 0 #unique id
+
+        for l in lists:
+          if l is not None:
+            heapq.heappush(minHeap, (l.val,count,l)) #klog k
+            count += 1
+        
+        while minHeap:
+            val, _, l  = heapq.heappop(minHeap) #nlogk
+
+            curr.next = l
+            curr = curr.next
+
+            if l.next is not None:
+                count += 1
+                heapq.heappush(minHeap, (l.next.val, count, l.next))
+        
+        return dummy.next
+
+
+        
+
+
+    # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+    #     if len(lists) == 0:
+    #         return None
+
+    #     def mergeTwoLists(l1, l2):
+
+    #         dummy = ListNode(0)
+    #         curr = dummy
+
+    #         while l1 and l2:
+    #             if l1.val < l2.val:
+    #                 curr.next = l1
+    #                 l1 = l1.next
+    #             else:
+    #                 curr.next = l2
+    #                 l2 = l2.next
+    #             curr = curr.next
+    #         curr.next = l1 or l2
+
+    #         return dummy.next
+
+
+    #     while (len(lists) > 1):
+    #         mergedLists = []
+    #         for i in range(0, len(lists),2):
+    #             l1 = lists[i]
+    #             l2 = lists[i + 1] if i + 1 < len(lists) else None
+    #             mergedLists.append(mergeTwoLists(l1,l2))
+    #         lists = mergedLists
+        
+    #     return lists[0]
+
+                
+
+    
+
+
+
+
+
+
+
+
+
+        
