ed532eb91a6454937964fe4a2ce68d8d61a1bd03
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-09-02 00:31:51 -0700

    Add: network-delay-time - submission-2

---
+++ Data Structures & Algorithms/network-delay-time/submission-2.py
@@ -0,0 +1,29 @@
+import collections
+import heapq
+
+class Solution:
+    #Time - ElogV or ELogV (Each Pop/Push) - LogE. Number of times - E
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edges = collections.defaultdict(list)
+        
+
+        for u, v, w in times:
+            edges[u].append((v, w))
+
+        distanceDict = {} #Finalized distances, also acts as visited set to keep track
+        minHeap = [(0, k)] #Contains distance, Node 
+
+        while minHeap:
+
+            dist, v = heapq.heappop(minHeap)
+
+            if v in distanceDict:
+                continue
+
+            distanceDict[v] = dist
+
+            for v2, dist2 in edges[v]:
+               if v2 not in distanceDict: 
+                heapq.heappush(minHeap, (dist + dist2, v2))
+
+        return max(distanceDict.values()) if len(distanceDict) == n else -1
