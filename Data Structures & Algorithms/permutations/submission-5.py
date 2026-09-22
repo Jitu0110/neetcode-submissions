c593d2ae823b165195bdce40d9505fb3d811565c
Jitendra Vasishta <53211153+Jitu0110@users.noreply.github.com>
2026-09-22 10:24:33 -0700

    Add: permutations - submission-5

---
+++ Data Structures & Algorithms/permutations/submission-5.py
@@ -0,0 +1,31 @@
+class Solution:
+    #Time - O(N × N!)
+    # Trick here is to send the picked information , using pick boolean List
+    # Output space - O(N × N!)
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        perm = []
+
+        def backtrack (pick: List[bool]):
+            if len(perm) == len(nums):
+                res.append(perm[:])
+                return
+            for i in range(len(nums)):
+                if not pick[i]:
+                    #Use the number
+                    perm.append(nums[i])
+                    pick[i] = True
+
+                    backtrack(pick)
+
+                    #Remove the number
+                    perm.pop()
+                    pick[i] = False
+
+        backtrack([False] * len(nums))
+
+        return res
+
+
+
+        
